5395451ed905bb1f14e381a44ffc86525afece0b
hamza-shabbir94 <hamza.shabbir.94.4@gmail.com>
2026-06-12 19:33:21 +0200

    modifying app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ AWS.config.update({ region: 'eu-central-1' });
     conn = await mysql.createConnection({
       host: 'testdb.ctueoqes4434.eu-central-1.rds.amazonaws.com',
       port: 3306,
-      database: 'testdb',
+      database: 'mysql',
       user: 'admin',
       password,
       ssl: { rejectUnauthorized: false, ca: require('fs').readFileSync('./global-bundle.pem') }
