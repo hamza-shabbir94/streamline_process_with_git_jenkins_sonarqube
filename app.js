010b852551a59abb87f263462fed8b3ac1612714
hamza-shabbir94 <hamza.shabbir.94.4@gmail.com>
2026-06-12 14:19:01 +0200

    modifying app.js

--- app.js
+++ app.js
@@ -11,10 +11,10 @@ const port = 3000;
 
 // Create connection to MySQL
 const db = mysql.createConnection({
-    host: 'testdb-1.c34egoce037f.ap-south-1.rds.amazonaws.com',
+    host: 'mysql -h testdb.ctueoqes4434.eu-central-1.rds.amazonaws.com',
     user: 'root',
-    password: '12345678',
-    database: 'testdb_1'
+    password: 'password',
+    database: 'testdb'
 });
 
 // Connect to MySQL
